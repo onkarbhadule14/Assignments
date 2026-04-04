2d636ea48562896899936a2783a36f100deefd31
onkarbhadule14 <onkarbhadule14@gmail.com>
2026-04-04 15:31:49 +0530

    updated code

---
+++ Assignments/data-type.ts
@@ -0,0 +1,38 @@
+// Assignment 1 – Data Types in TypeScript
+// 1. Temperature of a city in degrees Celsius: 25.5 
+let Temperature:number = 25.5;
+console.log("Temperature of the city: " + Temperature + "°C");
+
+// 2.a. Whether a customer has placed an order: true or false
+let hasPlacedOrder:boolean = true;
+console.log("Has the customer placed an order? " + hasPlacedOrder);
+
+// 2.b. Whether a customer has placed an order: true or false
+let hasPlacedOrder_b:boolean = false;
+console.log("Has the customer placed an order? " + hasPlacedOrder_b);
+
+// 3. Person's phone number: "123-456-7890"
+let phoneNumber: string = "123-456-7890";
+console.log("Person's phone number: " + phoneNumber);
+
+//4. Amount of money in a customer's bank account
+let bankAccountBalance: number = 1500.75;
+console.log("Amount of money in the customer's bank account: $" + bankAccountBalance);
+
+// 5. Person's email address 
+let emailAddress:string="abc@email.com";
+console.log("Person's email address: " + emailAddress);
+
+//6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+let  latitude:number=37.7749;
+let longitude:number=-122.4194;
+console.log("Coordinates of the location: Latitude " + latitude + ", Longitude " + longitude);
+
+//7. Person's marital status: true or false
+let isMarried:boolean=true;
+console.log("Is the person married? " + isMarried);
+
+//8. Person's occupation: "Software Engineer" 
+let occupation:string="Software Engineer";
+console.log("Person's occupation: " + occupation);
+
